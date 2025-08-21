85743c1277f4236731f4431ce317097ed70953a7
Happy-negi238 <happynegi238@gmail.com>
2025-08-21 10:48:48 -0700

    pdf form

--- static/js/certificate.js
+++ static/js/certificate.js
@@ -1,18 +1,33 @@
 let form = document.getElementById("form")
 let tag = document.querySelectorAll("#anchor")
-// console.log(tag);
-
 
 tag.forEach((a)=>{
-a.addEventListener("click",()=>{
+    a.addEventListener("click",()=>{
     // document.body.style.backgroundColor="red"
-    
     form.style.left="50%"
     form.style.transition="5s"
     form.style.display="block";
+    })
 })
 
-})
-
-// form.style.display="block";
+const validation = (input) => {
+    // console.log(input.value);  
+    let sibling = input.nextElementSibling;
+    
+    if(input.value.trim().length === 0){
+        input.style.borderBottomColor = "red";
+        input.style.setProperty("--bkl", "red");
+        sibling.style.visibility = "visible";
+        sibling.style.marginTop = "2px";
+        sibling.style.color = "red";
+        sibling.style.paddingLeft = "8px";
+        
+    }else{
+        input.style.borderBottomColor = "black";
+        input.style.setProperty("--bkl", "black");
+        sibling.style.visibility = "hidden";
+        sibling.style.marginTop = "0px";    
+        
+    }
+}
 
