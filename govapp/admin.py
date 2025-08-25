56e3804cf90b2dccfd615588764b92bd065bf439
Happy-negi238 <happynegi238@gmail.com>
2025-08-25 19:03:10 +0530

    nothing

--- govapp/admin.py
+++ govapp/admin.py
@@ -3,4 +3,5 @@ from .models import Projects
 # Register your models here.
 class ProjectsAdmin(admin.ModelAdmin):
     list_display = ('Image_url','Approved_Projects', 'New_Projects')
+    
 admin.site.register(Projects, ProjectsAdmin)
