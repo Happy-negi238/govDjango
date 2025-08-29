3569345ff19adfaded17aa4fdd17e79b9e5a059a
Happy-negi238 <happynegi238@gmail.com>
2025-08-29 16:19:39 +0530

    home page

--- govapp/views.py
+++ govapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Projects
+from django.db import connection
+from django.conf import settings
 # Create your views here.
 # def index(request):
 #     projectData = Projects.objects.all()
@@ -7,12 +9,18 @@ from .models import Projects
 
 
 def index(request):
-    projectData = Projects.objects.all()
-    grouped = []
-    size = 3
-    for i in range(0, len(projectData), size):
-        grouped.append(projectData[i:i+size])
-    return render(request, 'index.html', {'grouped': grouped})
+    with connection.cursor() as cursor:
+        lastToOne = Projects.objects.order_by('-id')
+        cursor.execute("Select Image_url from govapp_Projects order by id desc limit 5;")
+        data = cursor.fetchall()
+        imgagePath = []
+        for a in data:
+            imgagePath.append(settings.MEDIA_URL + a[0])
+        grouped = []
+        size = 3
+        for i in range(0, len(lastToOne), size):
+            grouped.append(lastToOne[i:i+size])
+    return render(request, 'index.html', {'grouped': grouped, 'projectData': lastToOne, 'data': imgagePath})
 
 def about(request):
     return render(request, 'about.html')
